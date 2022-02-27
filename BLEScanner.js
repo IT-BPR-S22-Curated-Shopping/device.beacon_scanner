a5143eec2abe94aebbe0891a855bea384698c494
jnorsted <jacob@norsted.dk>
2022-02-27 13:21:26 +0100

    Update BLEScanner.js

--- BLEScanner.js
+++ BLEScanner.js
@@ -20,24 +20,24 @@ function BLEScanner (configurationManager, uplinkHandler) {
             const beaconUUID = advertisement.iBeacon.uuid.split('-')
 
             if (configurationManager.getScannerConfig().filters.appId
-                && configurationManager.getScannerConfig().filters.companyId
-                && beaconUUID[0] === configurationManager.getAppId()
-                && beaconUUID[1] === configurationManager.getCompanyId()) {
-                console.log('App and Company matched')
-                console.log(advertisement)
-
+                && configurationManager.getScannerConfig().filters.companyId) {
+                if (beaconUUID[0] === configurationManager.getAppId()
+                    && beaconUUID[1] === configurationManager.getCompanyId()) {
+                    console.log('App and Company matched')
+                    console.log(advertisement)
+                }
             }
-            else if (configurationManager.getScannerConfig().filters.appId
-                && beaconUUID[0] === configurationManager.getAppId()) {
-                console.log('app matched')
-                console.log(advertisement)
-
+            else if (configurationManager.getScannerConfig().filters.appId) {
+                if (beaconUUID[0] === configurationManager.getAppId()) {
+                    console.log('app matched')
+                    console.log(advertisement)
+                }
             }
-            else if (configurationManager.getScannerConfig().filters.companyId
-                && beaconUUID[1] === configurationManager.getCompanyId()) {
-                console.log('Company matched')
-                console.log(advertisement)
-
+            else if (configurationManager.getScannerConfig().filters.companyId) {
+                if (beaconUUID[1] === configurationManager.getCompanyId()) {
+                    console.log('Company matched')
+                    console.log(advertisement)
+                }
             }
             else {
                 console.log('No filters applied')
