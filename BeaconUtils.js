c41471d0d127770a643b869a187e61433a4c5dd5
jnorsted <jacob@norsted.dk>
2022-02-27 16:39:11 +0000

    filter adjusted

--- BeaconUtils.js
+++ BeaconUtils.js
@@ -1,3 +1,3 @@
-const alpha = 0.4
-export const rssiToMeters = (txPower, rssi) => (10**((Number(txPower) - Number(rssi)) / (10 * 4))).toFixed(2)
+const alpha = 0.3
+export const rssiToMeters = (txPower, rssi) => (10**((Number(txPower) - Number(rssi)) / (10 * 3))).toFixed(2)
 export const calculateRssi = (lastRssi, currentRssi) => alpha * lastRssi + (1 - alpha) * currentRssi
