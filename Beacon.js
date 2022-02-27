f7263dd7790b14560c96689b8b59797a54425112
jnorsted <jacob@norsted.dk>
2022-02-27 17:28:10 +0100

    Update Beacon.js

--- Beacon.js
+++ Beacon.js
@@ -10,8 +10,7 @@ function Beacon(uuid, mac, major, minor) {
         time: Date.now(),
         updated: Date.now(),
         rssi: undefined,
-        distance: undefined,
-        observations: []
+        distance: undefined
     }
     const getDistance = () => state.distance
     const getRssi = () => state.rssi
@@ -19,22 +18,10 @@ function Beacon(uuid, mac, major, minor) {
         if (typeof state.rssi === 'undefined') {
             state.rssi = rssi
             state.distance = rssiToMeters(txPower, rssi)
-            state.observations.push(state.rssi)
-        }
-        else if (state.observations.length < 3) {
-            state.rssi = calculateRssi(state.rssi, rssi)
-            state.distance = rssiToMeters(txPower, state.rssi)
-            state.observations.push(state.rssi)
         }
         else {
-            let rssiSum = 0
-            state.observations.forEach((rssi) => {
-                rssiSum += rssi
-            })
-            state.rssi = calculateRssi(rssiSum / 3, rssi)
+            state.rssi = calculateRssi(state.rssi, rssi)
             state.distance = rssiToMeters(txPower, state.rssi)
-            state.observations.shift()
-            state.observations.push(state.rssi)
         }
         state.updated = Date.now()
     }
