89319af4cefdd92167aeb169a65fb7ec07bb44d0
jnorsted <jacob@norsted.dk>
2022-02-28 13:53:09 +0000

    sample filter adjusted

--- bluetooth/Beacon.js
+++ bluetooth/Beacon.js
@@ -13,17 +13,17 @@ function Beacon(uuid, mac, major, minor, rssi, distance) {
         observations: [ rssi ]
     }
     const addObservation = (rssi, txPower) => {
-        if (state.observations.length === 5) {
-            state.observations.shift()
-        }
-        state.observations.push(rssi)
         let rssiSum = 0
         state.observations.forEach((obs) => {
             rssiSum += obs
         })
-        const rssiAvg = rssiSum / 5
+        const rssiAvg = rssiSum / state.observations.length
         state.rssi = calculateRssi(rssiAvg, rssi)
         state.distance = rssiToMeters(txPower, state.rssi)
+        if (state.observations.length === 10) {
+            state.observations.shift()
+        }
+        state.observations.push(state.rssi)
         state.updated = Date.now()
     }
     const getUpdated = () => state.updated
