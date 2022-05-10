52759c2ca5bd849d0985bcfd6bd66c6180463aad
jnorsted <jacob@norsted.dk>
2022-05-10 18:30:51 +0200

    fix (BLEScanner) deactivate

--- bluetooth/BLEScanner.js
+++ bluetooth/BLEScanner.js
@@ -4,17 +4,15 @@ import { MessageLevel } from "../utils/MessageLevel.js"
 function BLEScanner(upLinkHandler) {
     const state = { scanner: new BeaconScanner() }
     const activate = () => state.scanner.startScan()
-        .then(() => upLinkHandler.sendTelemetry(MessageLevel.info, 'Scanning active'))
-        .catch((error) => {
-            upLinkHandler.sendTelemetry(MessageLevel.error, `Beacon scanner: ${error}`)
-            setTimeout(() => process.exit(1), 1000)
-        });
-    const deactivate = () => state.scanner.stopScan()
-        .then(() => upLinkHandler.sendTelemetry(MessageLevel.info, 'Scanning inactive'))
-        .catch((error) => {
-            upLinkHandler.sendTelemetry(MessageLevel.error, `Beacon scanner: ${error}`)
-            setTimeout(() => process.exit(1), 1000)
-        });
+    .then(() => upLinkHandler.sendTelemetry(MessageLevel.info, 'Scanning active'))
+    .catch((error) => {
+        upLinkHandler.sendTelemetry(MessageLevel.error, `Beacon scanner: ${error}`)
+        setTimeout(() => process.exit(1), 1000)
+    });
+    const deactivate = () => {
+        state.scanner.stopScan()
+        upLinkHandler.sendTelemetry(MessageLevel.info, 'Scanning inactive')
+    }
     return {
         activate, 
         deactivate
