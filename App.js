f5718aa0071cc75fb3b1af48e6fd46518bcc846f
jnorsted <jacob@norsted.dk>
2022-05-11 19:39:23 +0200

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import { Commands } from "./utils/Commands.js";
 import { State } from "./utils/State.js";
 import { hello } from "./models/Hello.js"
 import { status } from "./models/Status.js";
-import { deviceState } from "@abandonware/noble";
+import { deviceState } from "./utils/State.js";
 
 const configManager = ConfigurationManager(macAddress())
 const mqtt = Mqtt(configManager.getMqttConfig())
